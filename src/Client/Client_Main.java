0a432449024571735e100c548a2eaa3288ec3fc0
raide <raide@D0c_Laptop>
2023-06-20 21:16:25 -0400

    testing for creating threads

--- src/Client/Client_Main.java
+++ src/Client/Client_Main.java
@@ -1,10 +1,61 @@
 package Client;
 
+
+import java.net.InetAddress;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
 public class Client_Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
-
+		TestClass.dotest();
 	}
 
+}
+
+
+
+class TestClass {
+	public static void dotest(){
+		
+		try {
+			DatagramSocket s = new DatagramSocket();
+			
+			Scanner inFromUser = new Scanner(System.in);
+		    System.out.println("Please type your username: ");
+		    String username = inFromUser.nextLine();
+		    System.out.println("Please start typing your message and hit enter when you are done.");
+		    System.out.println("To exit the program, please type Quit.");
+		    String message = "";
+		    message = inFromUser.nextLine();
+		    
+		    while(!message.equals("Quit")) {
+	            
+	            // construct datagram packet
+		    	byte[] buf = message.getBytes();
+	            InetAddress address = InetAddress.getByName("127.0.0.1");
+	            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 194);
+	            
+	            
+	            // send the packet -- repeat until the user type "Quit"
+	            
+	            s.send(packet);
+	            System.out.println("Message sent, input next message or 'Quit'");
+	            message = inFromUser.nextLine();
+	
+	            
+	            }
+	            // close the datagram socket in the end. 
+	       s.close();
+	       inFromUser.close();
+	       
+		} catch (IOException e) {
+
+            System.err.print(e.getMessage());
+
+        }
+       
+	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class Server {
 
@@ -8,18 +9,24 @@ public class Server {
 		// TODO Auto-generated method stub
 		
 		boolean run = true;
+		int PORT = 194;
 		
-		DatagramSocket socket = new DatagramSocket(17);
+		DatagramSocket socket = new DatagramSocket(PORT);
+		DataManager serverData = new DataManager();
+		
 		System.out.println("Server started");
+		
 		
 		while(run){
 			try {
-				
+				System.out.println("in loop");
 				byte[] buffer = new byte[256];
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
 				socket.receive(request);
+				System.out.println("recieved");
 				System.out.println("Recieved connection at " + request.getAddress());
-				
+				System.out.println(request.getData().toString());
+				new ClientHandler(request, socket, serverData);
 				//
 				
 			}catch(Exception e){
@@ -28,21 +35,55 @@ public class Server {
 		
 		}
 		
-		
-		
 	}
 	
 	
 
 }
 
-class Handler implements Runnable {
+class DataManager{
 	
-	Handler(){
-		
+	ArrayList<String> global;
+	
+	DataManager(){
+		global = new ArrayList<String>();
+	}
+	
+	public void addToTextLog(String text) {
+		global.add(text);
+	}
+	
+	
+}
+
+class ClientHandler implements Runnable {
+	
+	DatagramSocket socket;
+	DatagramPacket packet;
+	boolean run;
+	DataManager manager;
+	
+	ClientHandler(DatagramPacket pkt, DatagramSocket sock, DataManager data){
+		this.packet = pkt;
+		this.socket = sock;
+		this.run = true;
+		this.manager = data;
+		new Thread(this).start();
+		System.out.println("created thread");
 	}
 	
 	public void run() {
+		try {
+			while(run) {
+				byte[] buffer = new byte[256];
+				this.packet = new DatagramPacket(buffer, buffer.length);
+				this.socket.receive(this.packet);
+				String text = new String(this.packet.getData(), 0, this.packet.getLength());
+				System.out.println("recieved: " + text);
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 }
