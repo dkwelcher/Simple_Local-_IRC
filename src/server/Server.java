f5b63b3f71468f41e9a5f8aa8cd4463bdb2ee4c2
raide <raide@Jamel_PC>
2023-06-24 16:19:41 -0400

    Ability to send chat history and user list

--- src/server/Server.java
+++ src/server/Server.java
@@ -19,11 +19,11 @@ public class Server {
 		
 		while(run){
 			try {
-				System.out.println("in loop");
+				
 				byte[] buffer = new byte[256];
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
 				socket.receive(request);
-				System.out.println("recieved");
+				
 				System.out.println("Recieved connection at " + request.getAddress());
 				
 				String text = new String(request.getData(), 0, request.getLength());
@@ -46,14 +46,14 @@ public class Server {
 class DataManager{
 	
 	ArrayList<String> global;
-	ArrayList<String[]> users;
+	ArrayList<String> users;
 	ArrayList<InetAddress> ips;
 	ArrayList<Integer> ports;
 	//make list of clients
 	
 	DataManager(){
 		global = new ArrayList<String>();
-		users = new ArrayList<String[]>();
+		users = new ArrayList<String>();
 		ips = new ArrayList<InetAddress>();
 		ports = new ArrayList<Integer>();
 	}
@@ -63,22 +63,33 @@ class DataManager{
 		
 	}
 	
-	public void attemptaddUser(DatagramPacket request) {
-		//String username = new String(request.getData());
-		//String ip = new String(request.getAddress().toString());
+	public boolean attemptaddUser(DatagramPacket request) {
 		
 		if(!ips.contains(request.getAddress())) {
 			ips.add(request.getAddress());
 			ports.add(request.getPort());
+			users.add((new String(request.getData(), 0, request.getLength()).split(" ")[0]));
 			addToTextLog(request.getAddress().toString() + "joined the room\n");
+			
+			return true;
+		}else {
+			return false;
 		}
+		
 	}
-	public ArrayList<String[]> getUsers(){
+	public ArrayList<String> getUsers(){
 		return users;
 	}
 	public ArrayList<InetAddress> getips(){
 		return ips;
 	}
+	public ArrayList<String> getChatLog(){
+		return global;
+	}
+	public ArrayList<Integer> getports(){
+		return ports;
+	}
+	
 	
 }
 
@@ -97,7 +108,10 @@ class ClientHandler implements Runnable {
 		this.socket = sock;
 		
 		this.manager = data;
-		this.manager.attemptaddUser(pkt);
+		
+		
+		
+		
 		
 		new Thread(this).start();
 		System.out.println("created thread");
@@ -108,17 +122,30 @@ class ClientHandler implements Runnable {
 	public void run() {
 		try {
 			
-			
+			if(this.manager.attemptaddUser(this.packet)) {
+				this.sendHistory(this.packet);
+			}
 			
 			String text = new String(this.packet.getData(), 0, this.packet.getLength());
 			try {
-				this.manager.addToTextLog(text.split(" ")[1]);
+				this.manager.addToTextLog(text);
 			}catch(Exception e) {
 				
 				this.manager.addToTextLog(text);
 			}
-			this.broadcast(this.packet, this.manager);
+			
+			
+			
+			if(text.contains("USER_REQUEST")) {
+				System.out.println("user data sent");
+				this.sendUserInfo(this.packet);
+			}else if(text.contains("GIVE_HISTORY")){
 				
+				this.sendHistory(this.packet);
+			}else{
+				System.out.println("chat history sent");
+				this.broadcast(this.packet, this.manager);
+			}
 				
 			
 		}catch(Exception e) {
@@ -128,27 +155,65 @@ class ClientHandler implements Runnable {
 	}//message, address, port
 	public void broadcast(DatagramPacket packet, DataManager data) throws UnknownHostException {
 		int i = 0;
-		System.out.println("broadcast reached");
+		System.out.println("broadcasting...");
 		for(InetAddress entry : this.manager.getips()){
 			
-			System.out.println("before");
+			
 			
 			try {
 				DatagramPacket toclient = new DatagramPacket(packet.getData(), packet.getLength(), 
 						entry, manager.ports.get(i));
 				this.socket.send(toclient);
-				System.out.println("attempted broadcast");
+				System.out.println("broadcast sent to " + entry + "/"+ manager.ports.get(i));
 			}catch(Exception e){
 				System.out.println(e.getMessage());
 				System.out.println("failed broadcast to" + entry);
 			}
-			System.out.println("attempted broadcast");
+			
 			i++;
 		}
 	}
 	
+	public void sendHistory(DatagramPacket packet) {
+		
+		for(String oldmessage : this.manager.getChatLog()) {
+			try {
+				byte[] data = oldmessage.getBytes();
+				DatagramPacket toclient = new DatagramPacket(data, data.length, 
+				packet.getAddress(), packet.getPort());
+				this.socket.send(toclient);
+				System.out.println("sending chat history message" + oldmessage);
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+				System.out.println("failed to send history message to" + packet.getAddress());
+			}
+		}
+		
+	}
 	
-	
+	public void sendUserInfo(DatagramPacket packet) {
+		ArrayList<String> userinfo = new ArrayList<String>();
+		int i = 0;
+		
+		for(String user : this.manager.getUsers()) {
+			String info = "DATA: " + user + " " + this.manager.getips().get(i).toString() + " " + this.manager.getports().get(i);
+			userinfo.add(info);
+			i++;
+		}
+		
+		for(String user : userinfo) {
+			try {
+				byte[] data = user.getBytes();
+				DatagramPacket toclient = new DatagramPacket(data, data.length, 
+										packet.getAddress(), packet.getPort());
+				this.socket.send(toclient);
+				System.out.println("sending user from list: " + user);
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+				System.out.println("failed to send username to " + packet.getAddress());
+			}
+		}
+	}
 }
 
 
